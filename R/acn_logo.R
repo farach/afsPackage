c9cfc8818ef35a087b3261eadadd2cf2b970def8
Alex <alexfarach@gmail.com>
2021-12-15 10:11:41 -0500

    fixed file names to pass pakage checks

--- R/acn_logo.R
+++ R/acn_logo.R
@@ -8,7 +8,7 @@
 
 acn_logo <- function() {
   grid::grid.raster(
-    magick::image_read("./images/accenture_logo_black.svg"),
+    magick::image_read(system.file("images/accenture_logo_black.svg", "afsPackage")),
     x = 0.05,
     y = 0.03,
     just = c("left", "bottom"),
