1cdad3216cd16c554e3d05e747b167700efe89be
Alex <alexfarach@gmail.com>
2021-12-15 06:49:45 -0500

    added: fonts, acn logo, new function for adding image to plot

--- R/theme_afs.R
+++ R/theme_afs.R
@@ -1,93 +1,122 @@
 #' theme_afs
 #' Creates AFS themed ggplot2 theme
 #'
-#' @param font_family Choose which font you want to use: "Classic", "Modern".
-#' Default is "Modern"
-#'
 #' @return ggplot object
 #' @export theme_afs
 #'
 #' @examples
 #' ggplot(cars, aes(speed, dist)) +
-#' geom_point(alpha = 0.75) +
-#' labs(title = "Cars Example Theme", subtitle = "Subtitle example",
-#' caption = "Caption Example\nMore lines here"
-#' ) +
-#' theme_afs()
-theme_afs <- function(font_family) {
-  theme_minimal()
-  theme(
-    panel.grid.minor = element_blank(),
-    panel.grid.major.y = element_line(
-      color = "#cbcbcb",
-      linetype = "dashed"
-    ),
-    panel.grid.major.x = element_line(
-      color = "#cbcbcb",
-      linetype = "dashed"
-    ),
-    panel.background = element_blank(),
-    strip.background = element_rect(color = "white", size = 3),
-    strip.text = element_text(
-      #hjust = 0,
-      color = "#4B636E",
-      size = 12
-    ),
-    axis.ticks = element_blank(),
-    plot.title = element_text(
-      # family = font,
-      size = 20,
-      face = "bold",
-      hjust = 0.5,
-      vjust = 2,
-      color = "#6a1c91"
-    ),
-    plot.subtitle = element_text(
-      # family = font,
-      size = 14,
-      color = "#6a1c91",
-      hjust = 0.5
-    ),
-    plot.caption = element_text(
-      # family = font,
-      size = 9,
-      hjust = 1,
-      color = "#6a1c91"
-    ),
-    axis.title = element_text(
-      # family = font,
-      size = 10,
-      color = "#4B636E"
-    ),
-    axis.text = element_text(
-      # family = font,
-      size = 9,
-      color = "#4B636E"
-    ),
-    axis.text.x = element_text(
-      margin = margin(5, b = 10)
-    ),
-    legend.text.align = 0,
-    legend.background = element_blank(),
-    legend.title = element_text(
-      # family = font,
-      size = 18,
-      color = "#4B636E"
-    ),
-    legend.key = element_blank(),
-    legend.text = element_text(
-      # family = font,
-      size = 18,
-      color = "#4B636E"
-    ),
-    plot.margin = unit(c(1, 1, 1.5, 1),"cm"),
-    axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0),
-                                angle = 90)
-  )
+#'   geom_point(alpha = 0.75) +
+#'   labs(
+#'     title = "Cars Example Theme", subtitle = "Subtitle example",
+#'     caption = "Caption Example\nMore lines here"
+#'   ) +
+#'   theme_afs()
+#'   acn_logo()
+library(showtext)
+
+# We need to load the Accenture fonts to use them in the plot.
+sysfonts::font_add(
+  family = "graphik-bold",
+  regular = "./fonts/Graphik TTF 161229/Graphik-Bold.ttf"
+)
+sysfonts::font_add(
+  family = "graphik-regular",
+  regular = "./fonts/Graphik TTF 161229/Graphik-Regular.ttf"
+)
+sysfonts::font_add(
+  family = "spectra-regular",
+  regular = "./fonts/GT_Sectra_Fine/GT-Sectra-Fine-Regular.otf"
+)
+
+showtext_auto()
+
+theme_afs <- function() {
+  theme_minimal() %+replace%
+    theme(
+      panel.grid.minor = element_blank(),
+      panel.grid.major.y = element_line(
+        color = "#cbcbcb",
+        linetype = "dashed"
+      ),
+      panel.grid.major.x = element_line(
+        color = "#cbcbcb",
+        linetype = "dashed"
+      ),
+      panel.background = element_blank(),
+      strip.background = element_rect(color = "white", size = 3),
+      strip.text = element_text(
+        color = "#7500c0",
+        family = "graphik-regular"
+      ),
+      axis.ticks = element_blank(),
+      plot.title = element_text(
+        size = 20,
+        color = "#460073",
+        hjust = 0.5,
+        margin = margin(0, 0, 10, 0),
+        family = "graphik-bold"
+      ),
+      plot.subtitle = element_text(
+        size = 14,
+        color = "#460073",
+        hjust = 0.5,
+        margin = margin(0, 0, 10, 0),
+        family = "spectra-regular"
+      ),
+      plot.caption = element_text(
+        size = 11,
+        color = "#460073",
+        margin = margin(20, 0, 0, 0),
+        family = "graphik-regular"
+      ),
+      legend.text.align = 0,
+      legend.background = element_blank(),
+      legend.title = element_text(
+        color = "#460073",
+        family = "graphik-regular"
+      ),
+      legend.key = element_blank(),
+      legend.text = element_text(
+        color = "#460073",
+        family = "graphik-regular"
+      ),
+      plot.margin = margin(25, 25, 10, 25),
+      axis.title = element_text(
+        family = "graphik-regular",
+        size = 10,
+        color = "#4B636E"
+      ),
+      axis.text = element_text(
+        family = "graphik-regular",
+        size = 9,
+        color = "#4B636E"
+      ),
+      axis.text.x = element_text(
+        color = "#96968c"
+      ),
+      axis.text.y = element_text(
+        color = "#96968c"
+      ),
+      axis.title.y = element_text(
+        margin = margin(0, 20, 0, 0),
+        color = "#96968c",
+        angle = 90
+      ),
+      axis.title.x = element_text(
+        margin = margin(20, 0, 0, 0),
+        color = "#96968c"
+      )
+    )
 }
 
-
-#logo <- image_read("../afs_logo.png")
-
-#p1
-#grid.raster(logo, x = 0.02, y = 0.01, just = c('left', 'bottom'), width = unit(1, 'inches'))
+acn_logo <- function() {
+  grid.raster(
+    image_read("./images/accenture_logo_black.svg"),
+    x = 0.05,
+    y = 0.03,
+    just = c("left", "bottom"),
+    width = unit(1, "inches"),
+    height = unit(0.28, "inches")
+  )
+}
