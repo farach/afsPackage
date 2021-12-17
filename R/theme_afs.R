b73f74b51322b193d50164c41b6b8fff1ae0133b
Alex <alexfarach@gmail.com>
2021-12-17 11:40:17 -0500

    updated package

--- R/theme_afs.R
+++ R/theme_afs.R
@@ -30,81 +30,79 @@ sysfonts::font_add(
 
 showtext::showtext_auto()
 
-library(ggplot2)
-
 theme_afs <- function() {
-  theme_minimal() %+replace%
-    theme(
-      panel.grid.minor = element_blank(),
-      panel.grid.major.y = element_line(
+  ggplot2::theme_minimal() %+replace%
+    ggplot2::theme(
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_line(
         color = "#cbcbcb",
         linetype = "dashed"
       ),
-      panel.grid.major.x = element_line(
+      panel.grid.major.x = ggplot2::element_line(
         color = "#cbcbcb",
         linetype = "dashed"
       ),
-      panel.background = element_blank(),
-      strip.background = element_rect(color = "white", size = 3),
-      strip.text = element_text(
+      panel.background = ggplot2::element_blank(),
+      strip.background = ggplot2::element_rect(color = "white", size = 3),
+      strip.text = ggplot2::element_text(
         color = "#7500c0",
         family = "graphik-regular"
       ),
-      axis.ticks = element_blank(),
-      plot.title = element_text(
+      axis.ticks = ggplot2::element_blank(),
+      plot.title = ggplot2::element_text(
         size = 20,
         color = "#460073",
         hjust = 0.5,
-        margin = margin(0, 0, 10, 0),
+        margin = ggplot2::margin(0, 0, 10, 0),
         family = "graphik-bold"
       ),
-      plot.subtitle = element_text(
+      plot.subtitle = ggplot2::element_text(
         size = 14,
         color = "#460073",
         hjust = 0.5,
-        margin = margin(0, 0, 10, 0),
+        margin = ggplot2::margin(0, 0, 10, 0),
         family = "spectra-regular"
       ),
-      plot.caption = element_text(
+      plot.caption = ggplot2::element_text(
         size = 11,
         color = "#460073",
-        margin = margin(20, 0, 0, 0),
+        margin = ggplot2::margin(20, 0, 0, 0),
         family = "graphik-regular"
       ),
       legend.text.align = 0,
-      legend.background = element_blank(),
-      legend.title = element_text(
+      legend.background = ggplot2::element_blank(),
+      legend.title = ggplot2::element_text(
         color = "#460073",
         family = "graphik-regular"
       ),
-      legend.key = element_blank(),
-      legend.text = element_text(
+      legend.key = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(
         color = "#460073",
         family = "graphik-regular"
       ),
-      plot.margin = margin(25, 25, 10, 25),
-      axis.title = element_text(
+      plot.margin = ggplot2::margin(25, 25, 10, 25),
+      axis.title = ggplot2::element_text(
         family = "graphik-regular",
         size = 10,
         color = "#4B636E"
       ),
-      axis.text = element_text(
+      axis.text = ggplot2::element_text(
         family = "graphik-regular",
         size = 9,
         color = "#4B636E"
       ),
-      axis.text.x = element_text(
+      axis.text.x = ggplot2::element_text(
         color = "#96968c"
       ),
-      axis.text.y = element_text(
+      axis.text.y = ggplot2::element_text(
         color = "#96968c"
       ),
-      axis.title.y = element_text(
+      axis.title.y = ggplot2::element_text(
         margin = margin(0, 20, 0, 0),
         color = "#96968c",
         angle = 90
       ),
-      axis.title.x = element_text(
+      axis.title.x = ggplot2::element_text(
         margin = margin(20, 0, 0, 0),
         color = "#96968c"
       )
